b6340afa3ba6db68e4f3892ca3c5222d4a3ab2e0
JaggerLaw <mamagotbig@gmail.com>
2025-11-11 14:07:08 +1300

    2 Added serialized fields for movement (play around)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float movementX = 0.0f;
+    [SerializeField] float movementY = 0.009f;
+    [SerializeField] float movementZ = 0.0f;
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(movementX, movementY, movementZ);
     }
 }

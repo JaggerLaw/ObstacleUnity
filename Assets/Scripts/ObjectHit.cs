a730e0654bfc411b54e74ad384691d84979d8376
JaggerLaw <mamagotbig@gmail.com>
2025-11-16 13:36:46 +1300

    5 added little score script to player object

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class ObjectHit : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Color meshColor = new Color(0.6588f, 0.1411f, 0.1451f, 1f);
     void OnCollisionEnter(Collision collision)
     {
-        GetComponent<MeshRenderer>().material.color = Color.maroon;
-        Debug.Log("You have hit: ");
+        GetComponent<MeshRenderer>().material.color = meshColor;
     }
 }

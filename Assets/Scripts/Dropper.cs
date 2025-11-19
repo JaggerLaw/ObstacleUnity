c0dd54a5b50da3ebe6adef90c28349d9ab937709
JaggerLaw <mamagotbig@gmail.com>
2025-11-20 03:43:16 +1300

    5 added falling object with timer to initiate fall

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float timeSinceGameStart = 0.0f;
+    public Rigidbody rigidbody;
+    [SerializeField] float timeForFall = 3f;
+    void Start()
+    {
+        Debug.Log("The game has started");
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceGameStart = Time.time;
+        Debug.Log("The time has elapsed");
+        Debug.Log(timeSinceGameStart + "Seconds");
+
+        if(timeSinceGameStart >= timeForFall)
+        {
+            rigidbody.useGravity = true;
+        }
+    }
+}

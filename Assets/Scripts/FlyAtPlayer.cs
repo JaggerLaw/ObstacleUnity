dfb6e4e422265a4eba47131fe22bff06d0a99a72
Jake G <mamagotbig@gmail.com>
2025-12-08 01:19:46 +1300

    8 destroy completed projectiles, cleaned up code

--- Assets/Scripts/FlyAtPlayer.cs
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -16,6 +16,21 @@ public class FlyAtPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MoveToPlayer();
+        DestroyWhenReached();
+    }
+
+    void MoveToPlayer()
+    {
         transform.position = Vector3.MoveTowards(transform.position, playerPos, speedOfProjectile * Time.deltaTime);
+        
+    }
+
+    void DestroyWhenReached()
+    {
+        if(transform.position == playerPos)
+        {
+            Destroy(gameObject);
+        }
     }
 }
